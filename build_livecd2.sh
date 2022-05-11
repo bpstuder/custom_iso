b7deba474efb4120aacfd3813ba35ca5001aeb97
Benoit-Pierre STUDER <37024586+bpstuder@users.noreply.github.com>
2022-05-11 09:54:05 +0200

    Updates

--- build_livecd2.sh
+++ build_livecd2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # MY_PATH="`dirname \"$0\"`"              # relative
-SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"  # absolutized and normalized
-if [ -z "$SCRIPT_PATH" ] ; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions re-evaled after suid)
-  exit 1  # fail
+# SCRIPT_PATH="$( (cd \"$SCRIPT_PATH\" && pwd))" # absolutized and normalized
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+if [ -z "$SCRIPT_PATH" ]; then
+    # error; for some reason, the path is not accessible
+    # to the script (e.g. permissions re-evaled after suid)
+    exit 1 # fail
 fi
 echo "## Running script from $SCRIPT_PATH ##"
 
@@ -15,13 +16,13 @@ echo "## Using ${CONFIG_FILE} ##"
 
 echo "## Installing prerequisites ##"
 sudo apt-get -qq install \
-                    squashfs-tools \
-                    schroot \
-                    genisoimage \
-                    xorriso \
-                    jq \
-                    curl \
-                    tree
+    squashfs-tools \
+    schroot \
+    genisoimage \
+    xorriso \
+    jq \
+    curl \
+    tree
 echo ">> Done"
 
 echo "## Checking config file presence ##"
@@ -35,7 +36,7 @@ if [[ -f ${CONFIG_FILE} ]]; then
     echo "Output iso : ${OUTPUT_ISO}"
     OUTPUT_VOLUME=$(echo ${JSON} | jq -r '.output_volume')
     echo "Output Volume : ${OUTPUT_VOLUME}"
-  
+
     # readarray -t FILES_ORIGIN < <(jq -r '.additional_files[].origin_path' ${CONFIG_FILE})
     # readarray -t FILES_TARGET < <(jq -r '.additional_files[].target_path' ${CONFIG_FILE})
     # readarray -t FILES_DESCRIPTION < <(jq -r '.additional_files[].description' ${CONFIG_FILE})
@@ -45,7 +46,7 @@ if [[ -f ${CONFIG_FILE} ]]; then
     # i=0
     # for FILES in "${FILES_DESCRIPTION[@]}"; do
     #     echo "Processing $FILES"
-        
+
     #     ((i++))
     # done
 else
@@ -59,39 +60,40 @@ if [[ ! -d $SCRIPT_PATH/livecd-${FLAVOUR} ]]; then
     echo "Creating folders"
     mkdir $SCRIPT_PATH/livecd-${FLAVOUR}
     cd $SCRIPT_PATH/livecd-${FLAVOUR}
-    mkdir iso squashfs out
+    mkdir iso squashfs out boot
     echo ">> Done"
 fi
 
 case $FLAVOUR in
-    ubuntu)
-        echo "Ubuntu flavour selected"
-        ISO_NAME="ubuntu-22.04-desktop-amd64.iso"
-        ISO_URL="https://releases.ubuntu.com/jammy/ubuntu-22.04-desktop-amd64.iso"
+ubuntu)
+    echo "Ubuntu flavour selected"
+    ISO_NAME="ubuntu-22.04-desktop-amd64.iso"
+    ISO_URL="https://releases.ubuntu.com/jammy/ubuntu-22.04-desktop-amd64.iso"
     ;;
-    kubuntu)
-        echo "Kubuntu flavour selected"
-        ISO_NAME="kubuntu-22.04-desktop-amd64.iso"
-        ISO_URL="https://cdimage.ubuntu.com/kubuntu/releases/22.04/release/kubuntu-22.04-desktop-amd64.iso"
+kubuntu)
+    echo "Kubuntu flavour selected"
+    ISO_NAME="kubuntu-22.04-desktop-amd64.iso"
+    ISO_URL="https://cdimage.ubuntu.com/kubuntu/releases/22.04/release/kubuntu-22.04-desktop-amd64.iso"
     ;;
-    xubuntu)
-        echo "Xubuntu flavour selected"
-        ISO_NAME="xubuntu-22.04-desktop-amd64.iso"
-        ISO_URL="https://cdimage.ubuntu.com/xubuntu/releases/22.04/release/xubuntu-22.04-desktop-amd64.iso"
+xubuntu)
+    echo "Xubuntu flavour selected"
+    ISO_NAME="xubuntu-22.04-desktop-amd64.iso"
+    ISO_URL="https://cdimage.ubuntu.com/xubuntu/releases/22.04/release/xubuntu-22.04-desktop-amd64.iso"
     ;;
-    lubuntu)
-        echo "Lubuntu flavour selected"
-        ISO_NAME="lubuntu-22.04-desktop-amd64.iso"
-        ISO_URL="https://cdimage.ubuntu.com/lubuntu/releases/22.04/release/lubuntu-22.04-desktop-amd64.iso"
+lubuntu)
+    echo "Lubuntu flavour selected"
+    ISO_NAME="lubuntu-22.04-desktop-amd64.iso"
+    ISO_URL="https://cdimage.ubuntu.com/lubuntu/releases/22.04/release/lubuntu-22.04-desktop-amd64.iso"
     ;;
-    budgie)
-        echo "Budgie flavour selected"
-        ISO_NAME="ubuntu-budgie-22.04-desktop-amd64.iso"
-        ISO_URL="https://cdimage.ubuntu.com/ubuntu-budgie/releases/22.04/release/ubuntu-budgie-22.04-desktop-amd64.iso"
-    ;;   
-    *)
-        echo "Please select between Ubuntu, Kubuntu, Xubuntu, Lubuntu, Budgie"
-        exit
+budgie)
+    echo "Budgie flavour selected"
+    ISO_NAME="ubuntu-budgie-22.04-desktop-amd64.iso"
+    ISO_URL="https://cdimage.ubuntu.com/ubuntu-budgie/releases/22.04/release/ubuntu-budgie-22.04-desktop-amd64.iso"
+    ;;
+*)
+    echo "Please select between Ubuntu, Kubuntu, Xubuntu, Lubuntu, Budgie"
+    exit
+    ;;
 esac
 
 cd $SCRIPT_PATH/livecd-${FLAVOUR}
@@ -105,6 +107,10 @@ if [[ ! -f ${ISO_NAME} ]]; then
     sudo mount -o loop ${ISO_NAME} /mnt
     echo "Copying files"
     sudo cp -a /mnt/. iso
+    echo "Extracting Boot image"
+    dd if=${ISO_NAME} bs=1 count=432 of=boot/boot_hybrid.img
+    echo "Extracting EFI image"
+    dd if=${ISO_NAME} bs=512 skip=7129428 count=8496 of=boot/efi.img
     echo "Unmounting ISO"
     sudo umount /mnt
     echo "Mounting squashfs"
@@ -117,7 +123,7 @@ if [[ ! -f ${ISO_NAME} ]]; then
 fi
 
 echo "## Preparing system for modification ##"
-sudo mount --bind /proc squashfs/proc 
+sudo mount --bind /proc squashfs/proc
 sudo mount --bind /sys squashfs/sys
 sudo mount -t devpts none squashfs/dev/pts
 # sudo mount --bind /dev squashfs/dev
@@ -162,25 +168,13 @@ echo "Entering chroot"
 sudo chroot squashfs /usr/bin/bash <<"EOT"
 
 
-add-apt-repository universe
-add-apt-repository multiverse
+add-apt-repository universe > /dev/null
+add-apt-repository multiverse > /dev/null
 apt-get -qq update
 
 dpkg --configure -a
 apt-get -qq dist-upgrade
-apt-get -qq install -y curl jq
-# apt-get update
-# apt-get install -y curl jq
-
-# if [[ $(dpkg-query --show --showformat='${db:Status-Status}\n' 'curl') -eq 'not-installed' ]]; then
-#     echo "[Error] curl not installed. Exiting."
-#     exit
-# fi
-
-# if [[ $(dpkg-query --show --showformat='${db:Status-Status}\n' 'jq') -eq 'not-installed' ]]; then
-#     echo "[Error] jq not installed. Exiting.
-#     exit
-# fi
+apt-get -qq install -y curl jq whois
 
 CONFIG_FILE="/tmp/config.json"
 
@@ -189,22 +183,19 @@ readarray -t REPOSITORIES_GPG < <(jq -r '.repositories[].gpg_signature' ${CONFIG
 readarray -t REPOSITORIES_URL < <(jq -r '.repositories[].url' ${CONFIG_FILE})
 readarray -t REPOSITORIES_LIST < <(jq -r '.repositories[].list_file' ${CONFIG_FILE})
 
-readarray -t PACKAGES_REMOTE < <(jq --arg type "remote" -r '.packages[] | select(.type == $type).name' ${CONFIG_FILE})
-
-# echo "Installing Zoom"
-# cd /tmp
-# wget -q https://zoom.us/client/latest/zoom_amd64.deb
-# apt-get -qq install -y ./zoom_amd64.deb
-# echo ">> Done"
+readarray -t PACKAGES_APT < <(jq --arg type "apt" -r '.packages[] | select(.type == $type).name' ${CONFIG_FILE})
+readarray -t PACKAGES_SNAP < <(jq --arg type "snap" -r '.packages[] | select(.type == $type).name' ${CONFIG_FILE})
 
 echo "Adding repositories"
 i=0
 for REPOSITORY in ${REPOSITORIES_NAME[@]}; do
+    REPOSITORY=$(echo $REPOSITORY | tr '[:upper:]' '[:lower:]')
     echo "Processing $REPOSITORY"
+    
     echo "Adding GPG"
-    # wget -O - ${REPOSITORIES_GPG[$i]} | apt-key add -
-    curl -s -L ${REPOSITORIES_GPG[$i]} | apt-key add -
-    # echo ">> Done"
+    wget -O- ${REPOSITORIES_GPG[$i]} | gpg --dearmor | tee /usr/share/keyrings/$REPOSITORY-archive-keyring.gpg
+    echo ">> Done"
+
     echo "Adding repository"
     echo ${REPOSITORIES_URL[$i]} | tee "/etc/apt/sources.list.d/${REPOSITORIES_LIST[$i]}" > /dev/null
     echo ">> Done"
@@ -215,11 +206,21 @@ apt-get -qq autoremove
 apt-get -qq autoclean
 echo ">> Done"
 
-echo "Installing remote packages"
+echo "Installing APT packages"
 i=0
-for PACKAGE_REMOTE in ${PACKAGES_REMOTE[@]}; do
-    echo "Processing $PACKAGE_REMOTE"
-    apt-get -qq install -y $PACKAGE_REMOTE
+for PACKAGE_APT in ${PACKAGES_APT[@]}; do
+    echo "Processing $PACKAGE_APT"
+    apt-get -qq install -y $PACKAGE_APT
+    echo ">> Done"
+    ((i++))
+done
+echo ">> Done"
+
+echo "Installing Snap packages"
+i=0
+for PACKAGE_SNAP in ${PACKAGES_SNAP[@]}; do
+    echo "Processing $PACKAGE_SNAP"
+    apt-get -qq install -y $PACKAGE_SNAP
     echo ">> Done"
     ((i++))
 done
@@ -245,21 +246,21 @@ echo "Exiting chroot"
 # umount -lf /proc
 # umount -lf /dev/pts
 # umount -lf /dev
-rm /etc/resolv.conf
+# rm /etc/resolv.conf
 # rm /etc/hosts
 exit
 EOT
 echo "Exited chroot"
 
 echo "Cleaning"
-sudo umount ./squashfs/proc 
+sudo umount ./squashfs/proc
 sudo umount ./squashfs/sys
 sudo umount ./squashfs/dev/pts
 echo ">> Done"
 
 echo "Generating new manifest"
 sudo chmod a+w iso/casper/filesystem.manifest
-sudo chroot squashfs dpkg-query -W --showformat='${Package}  ${Version}\n' > iso/casper/filesystem.manifest
+sudo chroot squashfs dpkg-query -W --showformat='${Package}  ${Version}\n' >iso/casper/filesystem.manifest
 sudo chmod go-w iso/casper/filesystem.manifest
 echo ">> Done"
 
@@ -269,26 +270,44 @@ echo ">> Done"
 
 echo "Creating the new squashfs"
 cd squashfs
-sudo mksquashfs . ../iso/casper/filesystem.squashfs > /dev/null #-info
+sudo mksquashfs . ../iso/casper/filesystem.squashfs >/dev/null #-info
 cd ..
 echo ">> Done"
 
 echo "Generating new ISO"
 cd iso
-sudo bash -c "find . -path ./isolinux -prune -o -type f -not -name md5sum.txt -print0 | xargs -0 md5sum | tee md5sum.txt" > /dev/null
+sudo bash -c "find . -path ./isolinux -prune -o -type f -not -name md5sum.txt -print0 | xargs -0 md5sum | tee md5sum.txt" >/dev/null
 echo ">> Done"
 
 echo "Generating disk image"
-# sudo genisoimage -o "Custom.iso" -r -J -no-emul-boot -V "USB_LINUX" -boot-load-size 4 -boot-info-table -b isolinux/isolinux.bin -c isolinux/boot.cat ./ 
-sudo xorriso -as mkisofs -r -V "${OUTPUT_VOLUME}" \
-             -cache-inodes -J -l \
-             -c isolinux/boot.cat \
-             -b isolinux/isolinux.bin \
-                -no-emul-boot -boot-load-size 4 -boot-info-table \
-             -eltorito-alt-boot \
-             -e boot/grub/efi.img \
-                -no-emul-boot -isohybrid-gpt-basdat \
-             -o $SCRIPT_PATH/livecd-${FLAVOUR}/out/${OUTPUT_ISO} ./
+# sudo genisoimage -o "Custom.iso" -r -J -no-emul-boot -V "USB_LINUX" -boot-load-size 4 -boot-info-table -b isolinux/isolinux.bin -c isolinux/boot.cat ./
+# sudo xorriso -as mkisofs -r -V "${OUTPUT_VOLUME}" \
+#              -cache-inodes -J -l \
+#              -c isolinux/boot.cat \
+#              -b isolinux/isolinux.bin \
+#                 -no-emul-boot -boot-load-size 4 -boot-info-table \
+#              -eltorito-alt-boot \
+#              -e boot/grub/efi.img \
+#                 -no-emul-boot -isohybrid-gpt-basdat \
+#              -o $SCRIPT_PATH/livecd-${FLAVOUR}/out/${OUTPUT_ISO} ./
+
+sudo xorriso -as mkisofs -r \
+    -V "${OUTPUT_VOLUME}" \
+    -o $SCRIPT_PATH/livecd-${FLAVOUR}/out/${OUTPUT_ISO} \
+    --grub2-mbr $SCRIPT_PATH/livecd-${FLAVOUR}/boot/boot_hybrid.img \
+    -partition_offset 16 \
+    --mbr-force-bootable \
+    -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b $SCRIPT_PATH/livecd-${FLAVOUR}/boot/efi.img \
+    -appended_part_as_gpt \
+    -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
+    -c '/boot.catalog' \
+    -b '/boot/grub/i386-pc/eltorito.img' \
+    -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info \
+    -eltorito-alt-boot \
+    -e '--interval:appended_partition_2:::' \
+    -no-emul-boot \
+    ./
+
 echo ">> Done"
 
 echo "Changing rights"
